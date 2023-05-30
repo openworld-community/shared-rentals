3675d0ee54a4db8f212548a558dd9ccd0c7019c5
Aleksey Anatskiy <uyiiio@gmail.com>
2023-05-30 15:00:45 +0400

    fix: SRT-18 import path for ormcomfig (#3)

--- sr-back/ormconfig.ts
+++ sr-back/ormconfig.ts
@@ -1,6 +1,6 @@
 import { DataSource } from 'typeorm';
-import loadConfig from 'config/database.config';
-import { TestMigration1684945119367 } from 'migrations/1684945119367-TestMigration';
+import loadConfig from './config/database.config';
+import { TestMigration1684945119367 } from './migrations/1684945119367-TestMigration';
 
 export const AppDataSource = new DataSource({
   type: 'postgres',
