e44aec5d052eb393a93dc01edf713ad752ffdbbe
sintell <uyiiio@gmail.com>
2023-06-09 17:47:45 +0400

    fix: enable autowire

--- sr-back/src/app/app.module.ts
+++ sr-back/src/app/app.module.ts
@@ -7,10 +7,13 @@ import appConfig from 'config/application.config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { App } from './entities/app.entity';
 import { logger } from 'src/middlewares/logger.middleware';
+import { AreaModule } from 'src/area/area.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
+      // First found variable takes precedence, so local env first
+      envFilePath: ['.env.local', '.env'],
       load: [appConfig, dbConfig],
     }),
     TypeOrmModule.forRootAsync({
@@ -19,12 +22,12 @@ import { logger } from 'src/middlewares/logger.middleware';
       useFactory: (configService: ConfigService) => ({
         type: 'postgres',
         ...configService.get<ConfigType<typeof dbConfig>>('database'),
-        // TODO: use autowire injector
-        entities: [App],
+        autoLoadEntities: true,
         migrations: [],
       }),
     }),
     TypeOrmModule.forFeature([App]),
+    AreaModule,
   ],
   exports: [TypeOrmModule],
   controllers: [AppController],
