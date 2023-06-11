d9ea2031e50b1e2064f3ea3901b3e4dc069a0837
sintell <uyiiio@gmail.com>
2023-06-11 18:00:39 +0400

    fix: specify migrations with glob

--- sr-back/ormconfig.ts
+++ sr-back/ormconfig.ts
@@ -1,10 +1,10 @@
 import { DataSource } from 'typeorm';
 import loadConfig from './config/database.config';
-import { TestMigration1684945119367 } from './migrations/1684945119367-TestMigration';
-import { Area1686312624644 } from './migrations/1686312624644-Area';
+import * as path from 'path';
 
 export const AppDataSource = new DataSource({
   type: 'postgres',
   ...loadConfig(),
-  migrations: [TestMigration1684945119367, Area1686312624644],
+  migrations: [path.join(__dirname, '/migrations/*{.ts,.js}')],
+  entities: [path.join(__dirname, '/src/**/entities/*{.ts,.js}')],
 });
