b3afe21087a47899b4fe6e8b61eb19f2ce2a6185
Aleksey Anatskiy <uyiiio@gmail.com>
2023-06-12 14:46:20 +0400

    fix: code review

--- sr-back/migrations/1686512532310-CreateUserTable.ts
+++ sr-back/migrations/1686512532310-CreateUserTable.ts
@@ -4,18 +4,18 @@ export class CreateUserTable1685958461570 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.startTransaction();
     queryRunner.query(`
-      CREATE TYPE "user_role" AS ENUM('user', 'moderator', 'admin);
+      CREATE TYPE "user_role" AS ENUM('user', 'moderator', 'admin');
     `);
 
     queryRunner.query(`
       CREATE TABLE IF NOT EXISTS "user" (
           "id" SERIAL PRIMARY KEY,
-          "first_name" VARCHAR(255) NOT NULL,
-          "last_name" VARCHAR(255) NOT NULL, 
+          "firstName" VARCHAR(255) NOT NULL,
+          "lastName" VARCHAR(255) NOT NULL, 
           "role" "user_role" NOT NULL DEFAULT 'user',
           "email" VARCHAR(255) UNIQUE,
-          "created_at" TIMESTAMP WITH TIME ZONE DEFAULT now(), 
-          "updated_at" TIMESTAMP WITH TIME ZONE DEFAULT now()
+          "createdAt" TIMESTAMP WITH TIME ZONE DEFAULT now(), 
+          "updatedAt" TIMESTAMP WITH TIME ZONE DEFAULT now()
         );
     `);
 
