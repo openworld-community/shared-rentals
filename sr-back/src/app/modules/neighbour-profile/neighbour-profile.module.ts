c503f1afc0ec16cc54895b39989e6f1e7624bd0e
Jus1x-by <juniorhdblr@gmail.com>
2023-06-26 17:46:55 +0300

    [SRT-20]: add missing userRepository

--- sr-back/src/app/modules/neighbour-profile/neighbour-profile.module.ts
+++ sr-back/src/app/modules/neighbour-profile/neighbour-profile.module.ts
@@ -3,9 +3,11 @@ import { NeighbourProfileController } from './neighbour-profile.controller';
 import { NeighbourProfileService } from './neighbour-profile.service';
 import { AreaModule } from 'src/area/area.module';
 import { UserModule } from '../user/user.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from '../user/entities/user.entity';
 
 @Module({
-  imports: [AreaModule, UserModule],
+  imports: [AreaModule, UserModule, TypeOrmModule.forFeature([User])],
   controllers: [NeighbourProfileController],
   providers: [NeighbourProfileService],
 })
